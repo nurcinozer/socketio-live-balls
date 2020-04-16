0c2703dfd5fa76a0e0766db50a34aae343cab3fa
nurçin özer <nurcinpiksel@outlook.com>
2020-04-16 23:00:17 +0300

    updated newUser event

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,5 +1,7 @@
 app.controller('indexController',['$scope', 'indexFactory', ($scope, indexFactory) => {
 
+    $scope.messages = [];
+
     $scope.init = () => {
         const username = prompt('please enter username');
 
@@ -18,6 +20,17 @@ app.controller('indexController',['$scope', 'indexFactory', ($scope, indexFactor
         indexFactory.connectSocket('http://localhost:3000', connectionOptions)
             .then((socket) => {
                 socket.emit('newUser', { username });
+
+                socket.on('newUser', (data) => {
+                    const messageData = {
+                        type: 0, // info
+                        username: data.username
+                    };
+
+                    $scope.messages.push(messageData);
+                    $scope.$apply();
+
+                });
             }).catch((err) => {
             console.log(err);
             });
